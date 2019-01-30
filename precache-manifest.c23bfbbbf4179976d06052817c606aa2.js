6411be0d2025eece698526297eca13834436ba75
Paolo Nicodemo <paolonicodemo@gmail.com>
2019-01-30 22:58:08 +0100

    Updates

---
+++ precache-manifest.c23bfbbbf4179976d06052817c606aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/judgeapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a40f1a97e763139678da",
+    "url": "/judgeapp/static/js/runtime~main.a40f1a97.js"
+  },
+  {
+    "revision": "baf2a274679ff71cea9d",
+    "url": "/judgeapp/static/js/main.baf2a274.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/judgeapp/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "baf2a274679ff71cea9d",
+    "url": "/judgeapp/static/css/main.4333c867.chunk.css"
+  },
+  {
+    "revision": "37096776bb7726164c22248d343fc81d",
+    "url": "/judgeapp/index.html"
+  }
+];
