de54313f1729fe65169f68aeee134a8f347ac8df
Paolo Nicodemo <paolonicodemo@gmail.com>
2019-02-06 22:49:23 +0100

    Added base configuration

---
+++ src/Config/Assi.js
@@ -0,0 +1,18 @@
+const Assi = {
+    X: 1,
+    P: 2,
+    Q: 3,
+    Y: 4,
+    Z: 5,
+    C: 6,
+    A: 7,
+    B: 8,
+    D: 9,
+    U: 10,
+    V: 11,
+    W: 12,
+    B2: 13,
+    D2: 14,
+}
+
+export default Assi;
---
+++ src/Config/Machine.js
@@ -0,0 +1,102 @@
+import Assi from "./Assi";
+
+
+const Machines = [{
+        Nome: "Jet",
+        Teste: 1,
+        Assi: [Assi.X, Assi.Y, Assi.Z, Assi.C, Assi.A],
+        Mandrini: "2+2",
+        Caricatore: true,
+        ModelloTcp: 1
+    },
+    {
+        Nome: "Smart",
+        Teste: 1,
+        Assi: [Assi.X, Assi.Y, Assi.Z, Assi.C, Assi.A, Assi.B],
+        Mandrini: "2+2",
+        Caricatore: false,
+        ModelloTcp: 1
+    },
+    {
+        Nome: "Twinjet",
+        Teste: 1,
+        Assi: [Assi.X, Assi.P, Assi.Q, Assi.Z, Assi.C, Assi.A],
+        Mandrini: "2+2",
+        Caricatore: true,
+        ModelloTcp: 1
+    },
+    {
+        Nome: "Twin",
+        Teste: 1,
+        Assi: [Assi.X, Assi.P, Assi.Q, Assi.Z, Assi.C, Assi.A],
+        Mandrini: "4",
+        Caricatore: false,
+        ModelloTcp: 2
+    },
+    {
+        Nome: "TwinTrc",
+        Teste: 1,
+        Assi: [Assi.X, Assi.P, Assi.Q, Assi.Z, Assi.C, Assi.A],
+        Mandrini: "4",
+        Caricatore: false,
+        ModelloTcp: 2
+    },
+    {
+        Nome: "Artist",
+        Teste: 1,
+        Assi: [Assi.X, Assi.P, Assi.Q, Assi.Z, Assi.C, Assi.A],
+        Mandrini: "2+2",
+        Caricatore: false,
+        ModelloTcp: 1
+    },
+    {
+        Nome: "Master",
+        Teste: 1,
+        Assi: [Assi.X, Assi.P, Assi.Q, Assi.Z, Assi.C, Assi.A],
+        Mandrini: "4",
+        Caricatore: false,
+        ModelloTcp: 1
+    },
+    {
+        Nome: "DoubleJet",
+        Teste: 2,
+        Assi: [Assi.X, Assi.Y, Assi.Z, Assi.B, Assi.A, Assi.C, Assi.U, Assi.V, Assi.W, Assi.P, Assi.Q, Assi.D],
+        Mandrini: "2+2",
+        Caricatore: true,
+        ModelloTcp: 3
+    },
+    {
+        Nome: "Double",
+        Teste: 2,
+        Assi: [Assi.X, Assi.Y, Assi.Z, Assi.B, Assi.A, Assi.C, Assi.U, Assi.V, Assi.W, Assi.P, Assi.Q, Assi.D],
+        Mandrini: "2+2",
+        Caricatore: false,
+        ModelloTcp: 3
+    },
+    {
+        Nome: "EvoJet",
+        Teste: 2,
+        Assi: [Assi.X, Assi.Y, Assi.Z, Assi.B, Assi.A, Assi.C, Assi.U, Assi.V, Assi.W, Assi.P, Assi.Q, Assi.D],
+        Mandrini: "2+2",
+        Caricatore: true,
+        ModelloTcp: 1
+    },
+    {
+        Nome: "EvoJetTRC",
+        Teste: 2,
+        Assi: [Assi.X, Assi.Y, Assi.Z, Assi.C, Assi.A, Assi.B, Assi.B2, Assi.U, Assi.V, Assi.W, Assi.P, Assi.Q, Assi.D],
+        Mandrini: "2+2",
+        Caricatore: false,
+        ModelloTcp: 1
+    },
+    {
+        Nome: "Evolution",
+        Teste: 2,
+        Assi: [Assi.X, Assi.Y, Assi.Z, Assi.B, Assi.A, Assi.C, Assi.U, Assi.V, Assi.W, Assi.P, Assi.Q],
+        Mandrini: "2+2",
+        Caricatore: false,
+        ModelloTcp: 3
+    },
+];
+
+export default Machines;
